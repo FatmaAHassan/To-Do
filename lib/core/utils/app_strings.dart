5d7a29f5c830fca090fe1fa37ebfd4aa2229c45b
Fatma Alaa <fatmalaa2002@gmail.com>
2024-11-09 01:19:42 +0200

    create structure

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,35 @@
+class AppSrings {
+  static const String appName = 'UpTodo';
+  static const String skip = 'Skip';
+  static const String next = 'Next';
+  static const String back = 'Back';
+  static const String getStarted = 'Get Stareted';
+  static const String onBordingTitleOne = 'Manage your tasks';
+  static const String onBordingSubTitleOne =
+      'You can easily manage all of your daily tasks in DoMe for free';
+  static const String onBordingTitleTwo = 'Create daily routine';
+  static const String onBordingSubTitleTwo =
+      'In Uptod  you can create your personalized routine to stay productive';
+  static const String onBordingTitleThree = 'Organaize your tasks';
+  static const String onBordingSubTitleThree =
+      'You can organize your daily tasks by adding your tasks into separate categories';
+  static const String today = 'Today';
+  static const String noTaskTitle = 'What do you want to do today?';
+  static const String noTaskSubTitle = 'Tap + to add your tasks';
+  static const String addTask = 'Add Task';
+  static const String title = 'Title';
+  static const String titleHint = 'Enter title here';
+  static const String note = 'Note';
+  static const String noteHint = 'Enter note here';
+  static const String date = 'Date';
+  static const String startTime = 'Start Time';
+  static const String endTime = 'End Time';
+  static const String createTask = 'Create Task';
+  static const String todo= 'TODO';
+  static const String completed = 'COMPLETED';
+  static const String taskCompleted = 'Task Completed';
+  static const String deleteTask = 'Delete Task';
+  static const String cancel = 'Cancel';
+  static const String color = 'Color';
+
+}
